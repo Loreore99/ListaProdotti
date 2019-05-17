dcd30627386942dfcec4ab80fa9415268fbb467a
Loreore99 <45238060+Loreore99@users.noreply.github.com>
2019-05-17 11:15:53 +0200

    Basic settings update

--- Lista/src/main/java/Service/UtenteService.java
+++ Lista/src/main/java/Service/UtenteService.java
@@ -4,5 +4,6 @@ public interface UtenteService {
 	public boolean Login(Utente utente);
 	public Utente Registrazione(Utente utente);
 	public Float SaldoDisp(Utente utente);
+	public boolean isAdmin(Utente utente);
 
 }
